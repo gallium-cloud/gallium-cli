8315ea845a0ac533f7a4b2217c74f113a90f238a
Richard Stephens <richard@gallium.cloud>
2026-02-25 12:47:09 +0100

    refactor: vm service fns are on a struct, reflect names in spec

--- src/api/vm_service_api/mod.rs
+++ src/api/vm_service_api/mod.rs
@@ -4,39 +4,43 @@ use anyhow::anyhow;
 #[allow(unused)]
 pub mod entities;
 
-pub async fn get_ws_url(
-    api_root_url: impl ToString,
-    access_token: impl ToString,
-    params: &GetWsUrlForVmServiceQueryParams,
-) -> anyhow::Result<String> {
-    let response = reqwest::Client::new()
-        .get(format!(
-            "{}/api/ws/ws_for_vm_service",
-            api_root_url.to_string()
-        ))
-        .query(params)
-        .header(
-            reqwest::header::AUTHORIZATION,
-            format!("Bearer {}", access_token.to_string()),
-        )
-        .header("Gallium-CLI", clap::crate_version!())
-        .send()
-        .await?;
+pub struct VmServiceApi;
 
-    if !response.status().is_success() {
-        anyhow::bail!(response.text().await.unwrap());
+impl VmServiceApi {
+    pub async fn get_ws_url_for_vm_service(
+        api_root_url: impl ToString,
+        access_token: impl ToString,
+        params: &GetWsUrlForVmServiceQueryParams,
+    ) -> anyhow::Result<String> {
+        let response = reqwest::Client::new()
+            .get(format!(
+                "{}/api/ws/ws_for_vm_service",
+                api_root_url.to_string()
+            ))
+            .query(params)
+            .header(
+                reqwest::header::AUTHORIZATION,
+                format!("Bearer {}", access_token.to_string()),
+            )
+            .header("Gallium-CLI", clap::crate_version!())
+            .send()
+            .await?;
+
+        if !response.status().is_success() {
+            anyhow::bail!(response.text().await.unwrap());
+        }
+
+        if let Some(msg) = response.headers().get("X-Gallium-Cli-Msg") {
+            eprintln!(
+                "{}",
+                std::str::from_utf8(msg.as_bytes()).expect("utf-8 msg header")
+            );
+        }
+
+        response
+            .json::<VncUrlResponse>()
+            .await?
+            .url
+            .ok_or_else(|| anyhow!("get_ws_url response missing url"))
     }
-
-    if let Some(msg) = response.headers().get("X-Gallium-Cli-Msg") {
-        eprintln!(
-            "{}",
-            std::str::from_utf8(msg.as_bytes()).expect("utf-8 msg header")
-        );
-    }
-
-    response
-        .json::<VncUrlResponse>()
-        .await?
-        .url
-        .ok_or_else(|| anyhow!("get_ws_url response missing url"))
 }
--- src/tasks/ssh.rs
+++ src/tasks/ssh.rs
@@ -1,5 +1,5 @@
 use crate::api::vm_service_api::entities::GetWsUrlForVmServiceQueryParams;
-use crate::api::vm_service_api::get_ws_url;
+use crate::api::vm_service_api::VmServiceApi;
 
 #[derive(clap::Parser)]
 pub(crate) struct SshArguments {
@@ -31,7 +31,7 @@ pub(crate) async fn ssh(gargs: &crate::args::GlobalArguments, args: &SshArgument
         }
     };
 
-    let ws_url = match get_ws_url(
+    let ws_url = match VmServiceApi::get_ws_url_for_vm_service(
         gargs.get_api_url(),
         &access_token,
         &GetWsUrlForVmServiceQueryParams {
