42957a13a4c9dd8943be05f1fc72f9026042a911
Richard Stephens <richard@gallium.cloud>
2026-02-25 16:56:01 +0100

    refactor: main rs action match

--- src/main.rs
+++ src/main.rs
@@ -14,41 +14,32 @@ async fn main() {
     let invocation = Invocation::parse();
 
     match invocation.action {
-        Some(Action::Proxy(args)) => {
-            return crate::tasks_internal::proxy::proxy(&args).await.unwrap()
-        }
-        Some(Action::Login) => return crate::tasks::login::login(&invocation.gargs).await.unwrap(),
-        Some(Action::Logout) => {
-            return crate::tasks::login::logout(&invocation.gargs)
-                .await
-                .unwrap()
-        }
-        Some(Action::Ssh(args)) => {
-            return crate::tasks::ssh::ssh(&invocation.gargs, &args)
-                .await
-                .unwrap()
-        }
-        _ => (),
-    };
-
-    //TODO: on windows, double-clicking the EXE from the file browser will result in a console window that immediately closes
-    match get_login_response_for_saved_credentials(&invocation.gargs).await {
-        Ok(login_resp) => {
-            if let Some(org) = login_resp.org {
-                println!("Logged in to Gallium org: {}", org.name);
-            }
-            if let Some(avail_orgs) = login_resp.available_orgs {
-                if !avail_orgs.is_empty() {
-                    println!("{} orgs available.", avail_orgs.len());
+        Some(Action::Proxy(args)) => crate::tasks_internal::proxy::proxy(&args).await,
+        Some(Action::Login) => crate::tasks::login::login(&invocation.gargs).await,
+        Some(Action::Logout) => crate::tasks::login::logout(&invocation.gargs).await,
+        Some(Action::Ssh(args)) => crate::tasks::ssh::ssh(&invocation.gargs, &args).await,
+        None => {
+            //TODO: on windows, double-clicking the EXE from the file browser will result in a console window that immediately closes
+            match get_login_response_for_saved_credentials(&invocation.gargs).await {
+                Ok(login_resp) => {
+                    if let Some(org) = login_resp.org {
+                        println!("Logged in to Gallium org: {}", org.name);
+                    }
+                    if let Some(avail_orgs) = login_resp.available_orgs {
+                        if !avail_orgs.is_empty() {
+                            println!("{} orgs available.", avail_orgs.len());
+                        }
+                    }
                 }
-            }
+                Err(_) => {
+                    println!(
+                        "Oops, you're not logged-in. Try `{:?} login`",
+                        std::env::current_exe().unwrap()
+                    );
+                }
+            };
+            Ok(())
         }
-        Err(_) => {
-            println!(
-                "Oops, you're not logged-in. Try `{:?} login`",
-                std::env::current_exe().unwrap()
-            );
-            return;
-        }
-    };
+    }
+    .unwrap();
 }
