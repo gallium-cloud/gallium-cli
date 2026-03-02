2d0eabdc7c476341df83e4ec754b8742d7d563ab
Richard Stephens <rjst01@gmail.com>
2026-03-02 12:30:01 +0100

    feat(import): add api fn for calling import endpoint

--- src/api/storage_api/mod.rs
+++ src/api/storage_api/mod.rs
@@ -1,7 +1,10 @@
 use crate::api::errors::ApiClientError;
 
 use crate::api::ApiClient;
-use crate::api::storage_api::entities::{DiskPoolListResponse, ListDiskPoolsPathParams};
+use crate::api::storage_api::entities::{
+    CmdSubmitResponse, DiskPoolListResponse, ImportNbdVolumePathParams, ListDiskPoolsPathParams,
+    VolumeNbdImportRequest,
+};
 use derive_more::Constructor;
 use reqwest::Method;
 use std::sync::Arc;
@@ -27,6 +30,29 @@ impl StorageApi {
             .send()
             .await?;
 
+        self.api_client.deser_response(response).await
+    }
+    pub async fn import_nbd_volume(
+        &self,
+        path_params: &ImportNbdVolumePathParams,
+        request_body: &VolumeNbdImportRequest,
+    ) -> Result<CmdSubmitResponse, ApiClientError> {
+        let response = self
+            .api_client
+            .request_authed(
+                Method::POST,
+                &[
+                    "cluster-api",
+                    &path_params.cluster_id,
+                    "volume",
+                    &path_params.kube_ns,
+                    "nbd",
+                    "import",
+                ],
+            )?
+            .json(request_body)
+            .send()
+            .await?;
         self.api_client.deser_response(response).await
     }
 }
